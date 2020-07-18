d4c6c0640e81e1d536ead5f0e761485c8efa32d5
pratyaksh320 <67832531+pratyaksh320@users.noreply.github.com>
2020-07-18 20:51:18 +0530

    Add files via upload

---
+++ bounceOffAlgorithm-master/sketch.js
@@ -0,0 +1,31 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "yellow";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "yellow";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  
+bounceoff(fixedRect,movingRect);
+
+
+  drawSprites();
+}
+
+
+
+
+
+
+
